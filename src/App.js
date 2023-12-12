4b0d3210d61a2ddb30803a6683d0254c1619d262
David Rajcher <david.rajcher@gmail.com>
2023-12-12 21:50:53 +0100

    images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav>
+        <input />
+        <button>Search</button>
+      </nav>
+      <section className='main'>
+        <div className='card'>
+          <img src={""} className='cardImage' alt='' />
+          <div className='card-header'>
+            <h2>Card title</h2>
+          </div>
+          <div className='card-body'>
+            <p>Card body</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
