0bc84bdb4f1625e21adc83e0ab462519a84f2880
David Rajcher <david.rajcher@gmail.com>
2023-12-20 19:26:17 +0100

    session 3

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import { Image } from "./Image";
 import { Body } from "./Body";
 
-const Card = ({ character }) => {
+const Card = ({ character, remove, removeFav, addFav, favorite }) => {
   const { full_name, super_powers, weaknesses } = character;
 
   return (
     <div className='card' key={character.full_name}>
-      <Image fullName={full_name} />
+      <Image
+        fullName={full_name}
+        removeFav={removeFav}
+        addFav={addFav}
+        favorite={favorite}
+      />
       <Body
         fullName={full_name}
         superPowers={super_powers}
         weaknesses={weaknesses}
       />
       <div className='button-container'>
-        <button className='secondary-button'>Remove</button>
+        <button className='secondary-button' onClick={() => remove(full_name)}>
+          Remove
+        </button>
       </div>
     </div>
   );
--- src/components/Card/Image.jsx
+++ src/components/Card/Image.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-export const Image = ({ fullName }) => {
+export const Image = ({ fullName, addFav, removeFav, favorite }) => {
+  console.log(fullName, favorite);
   return (
     <div className='image-container'>
       <img src={`images/${fullName}.jpeg`} className='cardImage' alt='' />
-      <button className='icon-button'>
-        <img src='/icons/heart-empty.png' alt='heart-empty' />
+      <button
+        className='icon-button'
+        onClick={() => (favorite ? removeFav(fullName) : addFav(fullName))}
+      >
+        {favorite ? (
+          <img src='/icons/heart-fill.png' alt='heart-fill' />
+        ) : (
+          <img src='/icons/heart-empty.png' alt='heart-empty' />
+        )}
       </button>
     </div>
   );
